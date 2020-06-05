d9b64446eb05660ed959b9c2e7cd02b748b033a4
Uilton Santos <uilton.santos@sky.uk>
2020-06-05 17:39:44 +0100

    Feature: Upload image on Server and Web

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -17,7 +17,7 @@ class PointsController {
     const transaction = await knex.transaction();
 
     const point = {
-      image: "https://images.unsplash.com/photo-1578916171728-46686eac8d58?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+      image: request.file.filename,
       name,
       email,
       whatsapp,
@@ -29,12 +29,16 @@ class PointsController {
     const insertedIds = await transaction("points").insert(point);
 
     const point_id = insertedIds[0];
-    const pointItems = items.map((item_id: number) => {
-      return {
-        item_id,
-        point_id,
-      };
-    });
+
+    const pointItems = items
+      .split(",")
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
+        return {
+          item_id,
+          point_id,
+        };
+      });
     await transaction("point_items").insert(pointItems);
 
     await transaction.commit();
@@ -57,7 +61,14 @@ class PointsController {
       .distinct()
       .select("points.*");
 
-    return response.json(points);
+    const serializedPoints = points.map((point) => {
+      return {
+        ...point,
+        image_url: `http://192.168.1.66:5000/uploads/${point.image}`,
+      };
+    });
+
+    return response.json(serializedPoints);
   }
 
   async show(request: Request, response: Response) {
@@ -69,6 +80,11 @@ class PointsController {
       return response.status(400).json({ message: "Point not found" });
     }
 
+    const serializedPoint = {
+      ...point,
+      image_url: `http://192.168.1.66:5000/uploads/${point.image}`,
+    };
+
     /**
      * SELECT * FROM items
      *     JOIN point_items ON items.id = point_items.item_id
@@ -79,7 +95,7 @@ class PointsController {
       .where("point_items.point_id", id)
       .select("items.title");
 
-    return response.json({ point, items });
+    return response.json({ point: serializedPoint, items });
   }
 }
 
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import multer from "multer";
+import multerConfig from "./config/multer";
 
 import PointsController from "./controllers/PointsController";
 import ItemsController from "./controllers/ItemsController";
 
 const routes = express.Router();
+const upload = multer(multerConfig);
+
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
@@ -11,8 +15,9 @@ const itemsController = new ItemsController();
 routes.get("/items", itemsController.index);
 
 // Points
-routes.post("/points", pointsController.create);
 routes.get("/points", pointsController.index);
 routes.get("/points/:id", pointsController.show);
+
+routes.post("/points", upload.single('image'), pointsController.create);
 
 export default routes;
