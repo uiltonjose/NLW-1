90f9cfb0b3ec60344a714f271cfc6f722f86eb34
Uilton Santos <uilton.santos@sky.uk>
2020-06-05 18:21:18 +0100

    Validating fields at Create API and update gitignore

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from 'cors';
-import routes from "./routes";
 import path from "path";
+import routes from "./routes";
+import { errors } from "celebrate";
 
 const app = express();
 
@@ -15,5 +16,7 @@ app.get("/", (request, response) => {
 app.use(routes);
 
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+
+app.use(errors());
 
 app.listen(5000);
